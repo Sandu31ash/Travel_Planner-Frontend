167a3b738b92962addd3b46fcd2ff0f812456cb3
Sandu31ash <hazinika253127ash@gmail.com>
2024-09-06 08:43:18 +0530

    implement home

--- src/Components/Home/Home.tsx
+++ src/Components/Home/Home.tsx
@@ -6,8 +6,24 @@ import { FaPlay } from "react-icons/fa";
 
 const Home = () => {
   return (
-    <div>
-        Home
+    <div className='Home container section'>
+        <div className="textDiv grid">
+            <span className='redText'>Find the best destinations easily in beautiful Sri Lanka</span>
+            <h2>Plan, Travel, Enjoy and be relax while your travel..</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consectetur quas suscipit perferendis veritatis dolorem quasi, dolor voluptates alias tempora rerum delectus cum saepe impedit! Voluptates quidem recusandae in consectetur iusto.</p>
+            <div className='buttons flex'>
+                <button className='btn'>SignIn</button>
+                <div className='playBtn flex'>
+                    <FaPlay className='icon'/>
+                    <span>Play Demo</span>
+                </div>
+            </div>
+        </div>
+
+        <div className='imgDiv flex'>
+            <img src={homeImg} alt="HomeImg" />
+        </div>
+
     </div>
   )
 }
